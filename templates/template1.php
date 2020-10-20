ca864325d006babd2f6ef239c347a97ec1a473cd
Insoluble-A <72034186+Insoluble-A@users.noreply.github.com>
2020-10-20 23:13:02 +0800

    Add files via upload

--- templates/template1.php
+++ templates/template1.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Document</title>
+  </head>
+  <body>
+    <header>
+      <?php  include '/includes/header.php' ;
 
+      ?>
+    </header>
+    <nav></nav>
+
+    <main>
+    </main>
+
+    <footer></footer>
+  </body>
+</html>
